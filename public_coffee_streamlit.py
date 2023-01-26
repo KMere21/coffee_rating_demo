1a0da752bd631408eb0fd245630e95ad463caef9
Kate Meredith <kmere21@gmail.com>
2023-01-26 14:16:09 -0500

    cleaning up page

--- public_coffee_streamlit.py
+++ public_coffee_streamlit.py
@@ -22,7 +22,7 @@ image0 = ('coffeebeans.jpg')
 st.image(image0)
 
 # insert page title
-st.title("Predicting Coffee Ratings from Review Data")
+st.title("Predict Coffee Ratings from Reviews")
 
 # insert subheader
 st.subheader("Project Background")
@@ -32,15 +32,14 @@ st.markdown("Online ratings and reviews are a valuable tool for helping consumer
 
 st.markdown("As of 2021, the global coffee market was valued at over 100 billion USD and growing (Businesswire). Increased concern about the social and environmental impact of products created a competitive market for high quality, responsibly sourced coffee (Research and Markets, 2022). This project primarily focuses on this subset of the industry, as the data is scraped from [CoffeeReview](https://www.coffeereview.com/). While reviews span 1997 to today, the site’s focus in the last 10 years has shifted to high quality coffee (Coffee Review, 2015).")
 
-st.markdown("At Coffee Review, the evaluation (cupping) is conducted by a small, trained team on unidentified coffee. The reviewer scores five subcomponents (aroma, acidity, body, flavor, and aftertaste) from 1-10 (higher is better). Evaluative and descriptive narrative is also provided. A final overall rating from 50-100 is given (higher is better). Top scores are in the 90s, with anything above 94 exceptional. More details on the reviewing process are available [here](https://www.coffeereview.com/how-coffee-review-works/).")
+st.markdown("At Coffee Review, the evaluation (cupping) is conducted by a small, trained team on unidentified coffee. The reviewer scores five subcomponents (aroma, acidity, body, flavor, and aftertaste) from 1-10. Evaluative and descriptive narrative is also provided. A final overall rating from 50-100 is given (in both cases, higher scores are better). Top scores are in the 90s, with anything above 94 exceptional. More details on the reviewing process are available [here](https://www.coffeereview.com/how-coffee-review-works/).")
 
-st.subheader("Test the Model!")
+st.subheader("Try the Text-Based Model!")
 
-st.markdown("Part of this project involved creating a text-based model to predict a coffee’s rating. The model was trained using only the text from the Blind Assessment portion of reviews posted before November 2022.") 
+st.markdown("Part of this project involved creating a text-based model to predict a coffee’s rating. **The model was trained using only the text from the *Blind Assessment* portion of reviews posted before November 2022.**") 
 
-st.markdown("Give it a try! Pick a review from [CoffeeReview](https://www.coffeereview.com/). Click into the review to see the Blind Assessment text. Copy and paste it into the box below. Any reviews posted November 2022 or later will be new to the model. You can see how well it does by comparing the prediction to the actual rating.")
+st.markdown("Give it a try! Pick a review from [CoffeeReview](https://www.coffeereview.com/). Click into the review to see the *Blind Assessment* text. Copy and paste it into the box below. **Any reviews posted November 2022 or later will be new to the model**. You can see how well it does by comparing the prediction to the actual rating.")
 
-st.write('[Latest Reviews](https://www.coffeereview.com/review/)')
 
 # Load the model using joblib
 model = joblib.load('./public_rating_pipeline.pkl')
@@ -55,11 +54,13 @@ if review != '':
     st.subheader('The predicted score is:') 
     st.header(np.round_(prediction).astype(int))
 else: 
-    st.write('Enter text above to get predicted score.')
+    st.write('Enter text above to get the predicted score. You can compare it to the actual score on Coffee Review.')
 
 
 # insert subheader
-st.subheader("Screenshots for Navigating Reviews")
+st.subheader("How to Find the Review Information")
+
+st.markdown("Go to the [latest reviews](https://www.coffeereview.com/). Click the *Read Complete Review* button. Then copy and paste the narrative text under *Blind Assessment*")
 
 # show where to click to see more of the review
 image1 = ('ReviewStep1.png')
